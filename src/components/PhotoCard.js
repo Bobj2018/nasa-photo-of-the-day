d0b515cf6940b7983b0f3c12a5d1e73a1df6d764
Joshua Rieth <joshua.m.rieth@gmail.com>
2019-11-22 00:03:11 -0500

    added date picker

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DateTime from 'react-datetime';
+import { Card, CardHeader, CardBody, CardText, Col } from 'reactstrap';
 
-export default function PhotoCard({title, date, hdurl, explanation}) {
-return (
-    <div>
-    <img src={hdurl}/>
-    <h2>{ title }</h2>
-    <p>{date}</p>
-    <p>{explanation}</p>
-    </div>
-)
+export default function PhotoCard({ title, date, hdurl, explanation, changeDate }) {
+	
+	return (
+		<Col>
+			<Card>
+				
+				<CardHeader>
+					<img src={hdurl} width="100%" />
+					<h2>{title}</h2>
+                    <DateTime
+					dateFormat="YYYY-MM-DD"
+					timeFormat={false}
+					onChange={(info) => {
+                        let setDate = `${info.get('year')}-${info.get('month')}-${info.get('date')}`
+                        console.info(setDate);
+                        changeDate(setDate)
+                       
+					}}
+				/>
+				</CardHeader>
+				<CardBody>
+					<CardText>{explanation}</CardText>
+				</CardBody>
+			</Card>
+		</Col>
+	);
 }
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,13 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import PhotoCard from './PhotoCard';
+import {Container, Row} from "reactstrap"
+
+//Timer
+import Moment from 'moment';
+import DateTime from 'react-datetime';
 
 export default function PhotoList() {
     const [photos, setPhotos] = useState([]);
-
+    const [ photoDate, setPhotoDate ] = useState('2012-03-14');
     const didUpdate = () => {
         axios
-        .get("http://api.nasa.gov/planetary/apod?api_key=obMZFZ3GvnlHOUa1HGsPGOMsLBd4gsCi9etteUTb")
+        .get(`http://api.nasa.gov/planetary/apod?api_key=obMZFZ3GvnlHOUa1HGsPGOMsLBd4gsCi9etteUTb&date=${photoDate}`)
         .then(res => {
             console.log(res.data);
             setPhotos(res.data);
@@ -15,13 +20,20 @@ export default function PhotoList() {
         .catch(err => console.log('Error: ', err));
     };
 
-    useEffect(didUpdate, []);
+    function changeDate(newDate) {
+        setPhotoDate(newDate)
+        console.log("Changing: ", newDate);
+        
+    }
+
+    useEffect(didUpdate, [photoDate]);
 
     return (
-        <div>
-            
-                <PhotoCard title={ photos.title } date={photos.date} hdurl={photos.hdurl} explanation={photos.explanation}/>
-            
-        </div>
+        <Container>
+            <Row>
+                
+                <PhotoCard title={ photos.title } date={photos.date} hdurl={photos.url} explanation={photos.explanation} changeDate={changeDate}/>
+                </Row>
+        </Container>
     )
 }
